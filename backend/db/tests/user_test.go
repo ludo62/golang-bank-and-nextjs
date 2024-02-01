86d1f88d511a419b2312e43969e258e8b465eb40
ludo62 <fournier.ludovic@orange.fr>
2024-02-01 16:18:23 +0100

    test fini avec update user

--- backend/db/tests/user_test.go
+++ backend/db/tests/user_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateUser(t *testing.T) {
+func createRandomUser(t *testing.T) db.User {
 	hashedPassword, err := utils.GenerateHashPassword(utils.RandomString(6))
 
 	if err != nil {
@@ -32,4 +32,40 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, arg.HashedPassword, user.HashedPassword)
 	assert.WithinDuration(t, user.CreatedAt, time.Now(), 2*time.Second)
 	assert.WithinDuration(t, user.UpdatedAt, time.Now(), 2*time.Second)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	user2, err := testQuery.CreateUser(context.Background(), db.CreateUserParams{
+		Email:          user1.Email,
+		HashedPassword: user1.HashedPassword,
+	})
+	assert.Error(t, err)
+	assert.Empty(t, user2)
+}
+
+func TestUpdateUser(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	newPassword, err := utils.GenerateHashPassword(utils.RandomString(8))
+
+	if err != nil {
+		log.Fatal("Impossible de générer un mot de passe hashé:", err)
+	}
+
+	arg := db.UpdateUserPasswordParams{
+		HashedPassword: newPassword,
+		ID:             user1.ID,
+		UpdatedAt:      time.Now(),
+	}
+	newUser, err := testQuery.UpdateUserPassword(context.Background(), arg)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, newUser)
+	assert.Equal(t, arg.HashedPassword, newUser.HashedPassword)
+	assert.Equal(t, user1.Email, newUser.Email)
+	assert.WithinDuration(t, user1.UpdatedAt, time.Now(), 2*time.Second)
 }
